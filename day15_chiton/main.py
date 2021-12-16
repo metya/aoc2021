8483a5cec7c7a02df747b6f02bde079c30f1a4af
metya <metya.tm@gmail.com>
2021-12-16 04:23:51 +0300

    little refactor of day 15

--- day15_chiton/main.py
+++ day15_chiton/main.py
@@ -1,71 +1,73 @@
 import os, sys
 import numpy as np
 from queue import PriorityQueue
-from collections import defaultdict
+from collections import defaultdict, deque
 
 task_dir = os.path.dirname(__file__)
 sys.path.append(f"{task_dir}/..")
 from get_tasks import get_input, check_example, generate_readme
 
 
-def get_dirs(center, i_size, j_size, going_up=False, going_down=False):
+def get_dirs(center, i_size, j_size):
     i, j = center
     up = max(0, i - 1), j
     left = i, max(0, j - 1)
     down = min(i + 1, i_size), j
     right = i, min(j_size, j + 1)
-    if going_up:
-        return up, left
-    if going_down:
-        return down, right
     return up, left, down, right
 
 
-def dijkstra(G, start=(0, 0), end=False, up=False, down=False):
+def dijkstra(G, start=(0, 0), end=False, pq=True, star=False):
     i_s, j_s = G.shape[0] - 1, G.shape[1] - 1
     end = (i_s, j_s) if not end else end
-    path = []
     parents = {}
-    visited = set()
-    pq = PriorityQueue()
+    q = PriorityQueue() if pq else deque()
     nodeCosts = defaultdict(lambda: float("inf"))
     nodeCosts[start] = 0
-    pq.put((0, start))
+    if pq:
+        q.put((0, start))
+    else:
+        q.append((0, start))
 
     def get_path(node):
+        path = []
         while node != start:
             path.append(node)
-            prev_node = parents[node]
-            node = prev_node
+            node = parents[node]
         path.append(start)
         return path[::-1]
 
-    while pq:
-        _, node = pq.get()
-        if node == end:
-            print("Get end!")
-            return get_path(node), nodeCosts[end]
-        visited.add(node)
-        for adjNode in get_dirs(node, i_s, j_s, up, down):
-            if adjNode in visited:
-                continue
+    def estimate(end, node):
+        return abs(end[0] - node[0]) + abs(end[1] - node[1])
+
+    acc = 0
+    while (q.qsize() if pq else len(q)) != 0:
+        acc += 1
+        _, node = q.get() if pq else q.popleft()
+        if pq and node == end:
+            print("With early stop!", acc)
+            return get_path(node), nodeCosts[node]
+        for adjNode in get_dirs(node, i_s, j_s):
             newCost = nodeCosts[node] + G[adjNode]
             if nodeCosts[adjNode] > newCost:
                 parents[adjNode] = node
                 nodeCosts[adjNode] = newCost
-                pq.put((newCost, adjNode))
-
-    print("Can't get to the end!")
-    return get_path(node), nodeCosts
+                if star:
+                    newCost += estimate(end, adjNode)
+                if pq:
+                    q.put((newCost, adjNode))
+                else:
+                    q.append((newCost, adjNode))
+    return get_path(end), nodeCosts[end]
 
 
 def part1(input: list[str]):
     ceiling = np.array([list(line) for line in input], dtype=int)
-    _, cost = dijkstra(ceiling, down=True)
+    _, cost = dijkstra(ceiling)
     print("The answer of part1 is:", cost)
 
 
-def part2(input: list[str]):
+def part2(input: list[str], pq=True, star=False):
     ceiling = np.array([list(line) for line in input], dtype=int)
     full_ceiling = ceiling.copy()
     for step in range(1, 5):
@@ -77,7 +79,7 @@ def part2(input: list[str]):
         down_ceiling = line_ceiling + step
         down_ceiling[down_ceiling > 9] -= 9
         full_ceiling = np.vstack([full_ceiling, down_ceiling])
-    _, cost = dijkstra(full_ceiling, down=True)
+    _, cost = dijkstra(full_ceiling, pq=pq, star=star)
     print("The answer of part2 is:", cost)
 
 
